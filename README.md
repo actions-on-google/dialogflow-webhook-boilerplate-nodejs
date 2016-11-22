5407ca757dae5604f3f11ffaa5213229d892828d
Sachit Mishra <sachitm@google.com>
2016-11-21 20:35:47 -0800

    Initial checkin of APIAI webhook template
    Initial checkin of App Engine app to fulfill API.AI business logic.
    
    Change-Id: I71ea07a1e2ca7e00f76dd894a19684323fe656e4

---
+++ README.md
@@ -0,0 +1,37 @@
+# Actions On Google Webhook Template Sample for Node.js
+==================================================
+
+This sample shows how to create a webhook for API.AI and Google Assistant
+
+
+## Setup Instructions
+
+# Pre-requisites
+ 1. API.AI: [https://api.ai](https://api.ai)
+
+See the developer guide and release notes at https://developers.google.com/actions/ for more details.
+
+# Steps:
+1. Create a new API.AI agent at http://api.ai
+1. Deploy the app to your preferred hosting environment (we recommend Google App Engine)
+1. In the Fulfillment page, enable Fulfillment and specify the deployment URL
+1. In the Headers section, add a key for "Google-Assistant-Signature", with your own private key value
+1. In any relevant intents, enable the webhook for the response
+
+## Documentation
+* Actions on Google: https://developers.google.com/actions/
+
+## References and How to report bugs
+* If you find any issues, please open a bug here on GitHub
+
+How to make contributions?
+Please read and follow the steps in the CONTRIBUTING.md
+
+License
+See LICENSE.md
+
+## Terms
+Your use of this sample is subject to, and by using or downloading the sample files you agree to comply with, the [Google APIs Terms of Service](https://developers.google.com/terms/) and the [Actions On Google Developer Terms of Service](https://developers.google.com/actions/docs/terms/).
+
+## Google+
+Actions on Google Developers Community on Google+ [http://g.co/actionsdev](http://g.co/actionsdev)
---
+++ app.js
@@ -0,0 +1,58 @@
+// Copyright 2016, Google, Inc.
+// Licensed under the Apache License, Version 2.0 (the 'License');
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an 'AS IS' BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+'use strict';
+
+process.env.DEBUG = 'actions-on-google:*';
+let Assistant = require('actions-on-google').ApiAiAssistant;
+let express = require('express');
+let bodyParser = require('body-parser');
+
+let app = express();
+app.use(bodyParser.json({type: 'application/json'}));
+
+const GOOGLE_HEADER_SIGNATURE = 'Google-Assistant-Signature';
+const PRIVATE_KEY = 'YOUR_PRIVATE_KEY';
+
+// [START YourAction]
+app.post('/', function (req, res) {
+  const assistant = new Assistant({request: req, response: res});
+  console.log('Request headers: ' + JSON.stringify(req.headers));
+  console.log('Request body: ' + JSON.stringify(req.body));
+
+  if (!assistant.isRequestFromApiAi(GOOGLE_HEADER_SIGNATURE, PRIVATE_KEY)) {
+    console.log('Request is not from trusted source (API.AI)');
+    return;
+  }
+
+  // Fulfill action business logic
+  function responseHandler (assistant) {
+    // Complete your fulfillment logic and send a response
+    assistant.tell('Hello, World!');
+  }
+
+  assistant.handleRequest(responseHandler);
+});
+// [END YourAction]
+
+if (module === require.main) {
+  // [START server]
+  // Start the server
+  let server = app.listen(process.env.PORT || 8080, function () {
+    let port = server.address().port;
+    console.log('App listening on port %s', port);
+  });
+  // [END server]
+}
+
+module.exports = app;
