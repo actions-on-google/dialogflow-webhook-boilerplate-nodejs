1e6be53ffa3df0be7b780b09b29e6f4ed9dfd519
Mandy Chan <manchan@google.com>
2018-03-29 15:57:56 -0700

    Convert to use client lib v2
    Change-Id: I035e973952ce372dad2cd355db77eedcae47c4bd

--- functions/index.js
+++ functions/index.js
@@ -13,23 +13,16 @@
 
 'use strict';
 
-process.env.DEBUG = 'actions-on-google:*';
-const { DialogflowApp } = require('actions-on-google');
+const {dialogflow} = require('actions-on-google');
 const functions = require('firebase-functions');
 
-exports.yourAction = functions.https.onRequest((request, response) => {
-  const app = new DialogflowApp({request, response});
-  console.log('Request headers: ' + JSON.stringify(request.headers));
-  console.log('Request body: ' + JSON.stringify(request.body));
+const app = dialogflow({debug: true});
 
-  // Fulfill action business logic
-  function responseHandler (app) {
-    // Complete your fulfillment logic and send a response
-    app.tell('Hello, World!');
-  }
-
-  const actionMap = new Map();
-  actionMap.set('input.welcome', responseHandler);
-
-  app.handleRequest(actionMap);
+app.intent('Default Welcome Intent', (conv) => {
+  conv.close('Hello, World!');
+  // Complete your fulfillment logic and
+  // send a response when the function is done executing
 });
+
+exports.yourAction = functions.https.onRequest(app);
+
