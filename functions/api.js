b0c1aebb7fb311f90cafd5edc1e6fbbc9f927aba
Hao Tran <hao.tran@go1.com>
2018-11-30 14:52:53 +0700

    Remove hard-coded api

--- functions/api.js
+++ functions/api.js
@@ -1,25 +1,13 @@
-const { baseUrl, jwt } = require('./auth');
+const { baseUrl } = require('./auth');
 const axios = require('axios');
 
 axios.defaults.baseURL = baseUrl;
-axios.defaults.headers.common.Authorization = `Bearer ${jwt}`;
 
-module.exports = axios;
-
-const { host, portal } = require('./config');
-
-function makeUrl(url) {
-  return host + url;
+function setJWT(jwt) {
+  axios.defaults.headers.common.Authorization = `Bearer ${jwt}`;
 }
 
 module.exports = {
-  get: (url) => {
-    console.log('url', makeUrl(url));
-    return axios.get(makeUrl(url));
-  },
-
-  post: (url, data) => {
-    console.log('url_post', makeUrl(url));
-    return axios.post(makeUrl(url), data);
-  }
+  api: axios,
+  setJWT,
 }
